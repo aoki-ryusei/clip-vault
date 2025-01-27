e0c5fc66891691351a55c324bff70f9d9356f64d
aoki-ryusei <r.aoooki27@gmail.com>
2025-01-27 15:28:06 +0900

    モデルのカラム情報を追加

--- src/spec/factories/token_storages.rb
+++ src/spec/factories/token_storages.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: token_storages
+#
+#  id         :bigint           not null, primary key
+#  expired_at :datetime
+#  token      :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 FactoryBot.define do
   factory :token_storage do
     id { 1 }
